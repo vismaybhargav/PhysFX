2dcf88762b8ff187c652dbf783c0ed3b24c6d9aa
Vismay Bhargav <vismay.bhargav@gmail.com>
2025-05-30 12:25:21 -0700

    wip: handle new property views in log view manager

--- src/main/java/org/vismayb/physfx/logging/LoggingView.kt
+++ src/main/java/org/vismayb/physfx/logging/LoggingView.kt
@@ -1,35 +1,54 @@
 package org.vismayb.physfx.logging
 
-import javafx.beans.property.Property
 import javafx.scene.Scene
 import javafx.scene.control.Label
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
+import javafx.beans.property.DoubleProperty
+import javafx.beans.property.FloatProperty
+import javafx.beans.property.IntegerProperty
+import javafx.beans.property.LongProperty
+import javafx.beans.property.BooleanProperty
+import javafx.beans.property.Property
+
+import org.vismayb.physfx.logging.ui.BasePropertyView
+import org.vismayb.physfx.logging.ui.BooleanPropertyView
+import org.vismayb.physfx.logging.ui.NumberBasePropertyView
+
 class LoggingView : Stage() {
     val root = VBox()
     val loggingScene = Scene(root)
-    val logEntries = mutableMapOf<String, Label>()
+    val logEntries = mutableMapOf<String, BasePropertyView<*>>()
 
     init {
         title = "Logging"
         width = 800.0
         height = 600.0
+        isAlwaysOnTop = true
         scene = loggingScene
-
-        // String --> String
     }
 
-    fun addListener(path: String, value: Property<*>) {
-        value.addListener { _, _, newValue ->
-            if(logEntries.containsKey(path)) {
-                println("Updating log entry for $path with new value: $newValue")
-                logEntries[path]?.text = "$path: $newValue"
-            } else {
-                println("Adding new log entry for $path with value: $newValue")
-                logEntries[path] = Label("$path: $newValue")
-                root.children.add(logEntries[path])
+    fun addEntry(path: String, value: Property<*>) {
+        if(!logEntries.containsKey(path)) {
+            // Check if value is a BooleanProperty
+            val entryView: BasePropertyView<*> = when (value) {
+                is BooleanProperty -> {
+                    BooleanPropertyView(path, value.get())
+                }
+                is DoubleProperty, is IntegerProperty, is FloatProperty, is LongProperty -> {
+                    NumberBasePropertyView(path, value.value)
+                }
+                else -> {
+                    throw IllegalArgumentException("Unsupported property type for path: $path")
+                }
             }
+            logEntries[path] = entryView
+            root.children.add(logEntries[path])
+        }
+
+        value.addListener { _, _, newValue ->
+            logEntries[path]?.updateValue()
         }
     }
 }
